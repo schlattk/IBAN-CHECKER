c54201f2308dc1fab625e19961b25bb2649a1891
konrad schlatte <konradschlatte@konrads-MacBook-Air.local>
2017-10-21 17:17:54 +0100

    simplified transformation, removed index as not needed, finished checker functionality and removed unnecessary tests

--- spec/ibanSpec.js
+++ spec/ibanSpec.js
@@ -6,21 +6,12 @@ describe('iban_checker', function(){
   it ('has a transformed number',function(){
       expect(iban_checker.transformed_iban).toBeDefined();
   });
-  it('has an index', function(){
-      expect(iban_checker.index).toBeDefined();
-  });
   it ('has a valid/invalid variable',function(){
       expect(iban_checker.valid).toBeDefined();
   });
-  it('has a number value associated to a letter key', function(){
-      expect(iban_checker.index['C']).toBe(12);
-  });
-  it('has a number value associated to a number key', function(){
-      expect(iban_checker.index['1']).toBe(1);
-  });
 });
 
-describe('checks whether IBAN is correct', function(){
+describe('transforms the IBAN', function(){
   iban_checker = new Iban_checker();
   iban_checker.transform('DE19760300800930875295');
   it('converts the mixed string into a transformed string ', function(){
@@ -28,13 +19,29 @@ describe('checks whether IBAN is correct', function(){
   });
 });
 
-describe('checks whether IBAN is correct', function(){
-      iban_checker1 = new Iban_checker();
-      iban_checker1.transform('GB82WEST12345698765432');
-          it('converts the mixed string into a transformed string ', function(){
-          expect(iban_checker1.transformed_iban).toBe('3214282912345698765432161182');
-          });
+describe('transform the IBAN', function(){
+  iban_checker1 = new Iban_checker();
+  iban_checker1.transform('GB82WEST12345698765432');
+  it('converts the mixed string into a transformed string ', function(){
+      expect(iban_checker1.transformed_iban).toBe('3214282912345698765432161182');
+    });
   });
+
+  describe('calculate remainder', function(){
+    iban_checker2 = new Iban_checker()
+    iban_checker2.check('GB82WEST12345698765432');
+    it('returns true if the remainder is 1', function(){
+      expect(iban_checker2.valid).toEqual(true);
+    })
+  })
+  describe('calculate remainder', function(){
+    iban_checker3 = new Iban_checker()
+    iban_checker3.check('DE59760300800930875295');
+    it('returns false if the remainder is not equal to 1', function(){
+      expect(iban_checker3.valid).toEqual(false);
+    })
+  })
+
 
     /*it('increases the balance when money is added', function(){
     account.deposit(1000);
--- src/iban.js
+++ src/iban.js
@@ -4,50 +4,21 @@ var Iban_checker = function(){
   this.iban = '0';
   this.transformed_iban = '1';
   this.valid = true;
-  this.index = {A:10,
-                B:11,
-                C:12,
-                D:13,
-                E:14,
-                F:15,
-                G:16,
-                H:17,
-                I:18,
-                J:19,
-                K:20,
-                L:21,
-                M:22,
-                N:23,
-                O:24,
-                P:25,
-                Q:26,
-                R:27,
-                S:28,
-                T:29,
-                U:30,
-                V:31,
-                W:32,
-                X:33,
-                Y:34,
-                Z:35,
-                1:1,
-                2:2,
-                3:3,
-                4:4,
-                5:5,
-                6:6,
-                7:7,
-                8:8,
-                9:9,
-                0:0};
-  };
-
+};
   Iban_checker.prototype.transform = function(number){
     this.iban = number;
-    var sliced = this.iban.slice(0,4);
-    this.transformed_iban = this.iban.slice(4) + sliced;
-    var mixed_array = this.transformed_iban.split('');
-    var number_array = [];
-    number_array = mixed_array.map(v => this.index[v]);
-    this.transformed_iban = number_array.join('');
+    this.transformed_iban = (this.iban.slice(4) + this.iban.slice(0,4)).split('').map(v => isNaN(v) ? (v.charCodeAt(0) & 255)- 55 : v).join('');
   };
+  Iban_checker.prototype.check = function(number){
+    this.transform(number);
+    const divisor = 97;
+    var divident = this.transformed_iban.slice(0,9);
+    var remainder = divident % divisor;
+    divident = remainder + this.transformed_iban.slice(9,16);
+    remainder = divident % divisor;
+    divident = remainder + this.transformed_iban.slice(16,23);
+    remainder = divident % divisor;
+    divident = remainder + this.transformed_iban.slice(23,30);
+    remainder = divident % divisor;
+    remainder != 1 ? this.valid = false : this.valid = true;
+  }
