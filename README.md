4907e8fde8cfa17e901a334d6bda4416c8e2cc7a
konrad schlatte <konradschlatte@konrads-MacBook-Air.local>
2017-10-23 16:42:20 +0100

    some refactoring, updated Readme

--- README.md
+++ README.md
@@ -2,4 +2,24 @@
 
 This apps takes as an input the IBAN (International Bank Account Number) and
 checks wether it is valid using an algorithm given by the international banking
-authority. It returns either a "Valid" or an "Invalid" statement
+authority.
+
+Implemented using the prototype pattern
+
+How to use:
+
+Clone the repo
+Open SpecRunner.html and iban_checker.html in the browser
+
+all tests(jasmine) should be passing
+
+iban = new Iban_checker;
+  Iban_checker {iban: "0", transformed_iban: "1", valid: true, divisor: 97}
+iban.check('AL35202111090000000001234567');
+  undefined
+iban.valid
+  true
+iban.check('AL35202111090000000001234561');
+ undefined
+iban.valid
+ false
--- src/iban.js
+++ src/iban.js
@@ -4,21 +4,19 @@ var Iban_checker = function(){
   this.iban = '0';
   this.transformed_iban = '1';
   this.valid = true;
+  this.divisor = 97;
 };
   Iban_checker.prototype.transform = function(number){
-    this.iban = number;
+    this.iban = number.toString();
     this.transformed_iban = (this.iban.slice(4) + this.iban.slice(0,4)).split('').map(v => isNaN(v) ? (v.charCodeAt(0) & 255)- 55 : v).join('');
   };
   Iban_checker.prototype.check = function(number){
     this.transform(number);
-    const divisor = 97;
-    var divident = this.transformed_iban.slice(0,9);
-    var remainder = divident % divisor;
-    divident = remainder + this.transformed_iban.slice(9,16);
-    remainder = divident % divisor;
-    divident = remainder + this.transformed_iban.slice(16,23);
-    remainder = divident % divisor;
-    divident = remainder + this.transformed_iban.slice(23,30);
-    remainder = divident % divisor;
+    var divident = this.transformed_iban.slice(0,10);
+    var remainder = divident % this.divisor;
+    divident = remainder + this.transformed_iban.slice(10,20);
+    remainder = divident % this.divisor;
+    divident = remainder + this.transformed_iban.slice(20,30);
+    remainder = divident % this.divisor;
     remainder != 1 ? this.valid = false : this.valid = true;
   }
